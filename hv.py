42d9000dcceb8dded2363f59fc1d281192d3802b
Vova <vovakr43@gmail.com>
2022-09-30 15:51:56 +0300

    end

--- hv.py
+++ hv.py
@@ -4,6 +4,6 @@ def func():
 
     print("Вывод случайного числа при помощи использования random.random()")
     print(random.random())
-    print("fffff");
-    print(random.random()*1000);
-    return 0;
+    print("fffff")
+    print(random.random()*1000)
+    return 0
